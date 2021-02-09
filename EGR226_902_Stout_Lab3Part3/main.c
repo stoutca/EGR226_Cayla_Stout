6badd698736cbdde0619cfa854032b74dde9a351
stoutca <stoutca@mail.gvsu.edu>
2021-02-09 16:14:16 -0500

    Lab 3 Parts 1,2, and 3 Code

---
+++ EGR226_902_Stout_Lab3Part3/main.c
@@ -0,0 +1,22 @@
+#include "msp.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	//Configure GPIO for output on P1.0 LED1 on MSP432 Launchpad
+	P1->DIR = BIT0;
+
+	//Temporary variable for loop-maintenance
+	int i,x;
+	x = 0;
+	while(1){
+	    P1->OUT ^= BIT0; //toggle LED status
+	    for(i = x; i>0; i--); //Crude delay
+	}
+
+}
